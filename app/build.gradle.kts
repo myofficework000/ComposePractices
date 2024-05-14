e78bd92018aae85a432c02dd4b2784c4c1e6c159
myofficework000 <cheetahmail007@gmail.com>
2024-05-15 03:01:45 +0530

    Added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation ("androidx.constraintlayout:constraintlayout-compose:1.0.1")
+    implementation ("androidx.compose.material:material-icons-extended:1.6.7")
+
 }
