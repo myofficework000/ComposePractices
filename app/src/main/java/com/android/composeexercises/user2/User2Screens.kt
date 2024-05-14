62e4c8989a406b9d417a2c5caf004ab2a6f3f5d6
myofficework000 <cheetahmail007@gmail.com>
2024-05-15 02:58:02 +0530

    Added user1 and user 2

---
+++ app/src/main/java/com/android/composeexercises/user2/User2Screens.kt
@@ -0,0 +1,9 @@
+package com.android.composeexercises.user2
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun User2Screens(modifier: Modifier = Modifier) {
+
+}
